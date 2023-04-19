4bac70d3bdd51d0c21589688fe4519e5a7bd51f1
lm101845 <1018457683@qq.com>
2023-04-19 11:42:03 +0800

    第1-2集看完

---
+++ 10-珠峰/react-systematic/demo/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   <div>珠峰培训</div>
+  </React.StrictMode>
+);
+
